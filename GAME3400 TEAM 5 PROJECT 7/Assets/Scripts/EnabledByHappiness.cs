9010df337beb478a114d75f975b722515111a7ed
Ryan Gehrlein <ryan.gehrlein@gmail.com>
2022-03-23 15:32:12 -0400

    Quick rename

--- GAME3400 TEAM 5 PROJECT 7/Assets/Scripts/EnabledByHappiness.cs
+++ GAME3400 TEAM 5 PROJECT 7/Assets/Scripts/EnabledByHappiness.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnableByHappiness : MonoBehaviour
+public class EnabledByHappiness : MonoBehaviour
 {
     [SerializeField]
     private bool enableWhenMatching = true;
